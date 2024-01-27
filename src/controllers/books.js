cc8e6469f1c80b12a0ebe2a4625f8f45941a5e78
karnaukhoff <n4echduf@gmail.com>
2024-01-28 01:22:40 +0300

    Книги готовы

--- src/controllers/books.js
+++ src/controllers/books.js
@@ -1,17 +1,66 @@
+const Book = require("../models/books")
+
+//get all Books
 const getBooks = (request, response) => {
-    //get all Books
+    Book.find({})
+    .then((book) => {
+      response.status(200).send(book);
+    })
+    .catch((e) => {
+      response.status(500).send(e.message);
+    });
 };
+
+//get a Book
 const getBook = (request, response) => {
-    //get a Book
+    const { book_id } = request.params;
+    Book.findById(book_id)
+      .then((book) => {
+        if (!book) response.status(404).send("Book not found");
+        else response.status(200).send(book);
+      })
+      .catch((e) => {
+        response.status(500).send(e.message);
+      });
 };
+
+//create a Book
 const createBook = (request, response) => {
-    //create a Book
+    const data = request.body;
+    Book.create(data)
+      .then((book) => {
+        response.status(201).send(book);
+      })
+      .catch((e) => {
+        response.status(500).send(e.message);
+      });
 };
+
+//update a Book
 const updateBook = (request, response) => {
-    //update a Book
+    const { book_id } = request.params;
+    const data = request.body;
+    Book.findByIdAndUpdate(book_id, data, { new: true, runValidators: true })
+      .then((book) => {
+        if (!book) response.status(404).send("Book not found");
+        else response.status(200).send(book);
+      })
+      .catch((e) => {
+        response.status(500).send(e.message);
+      });
 };
+
+//delete a book
 const deleteBook = (request, response) => {
-    //delete a Book
+    const { book_id } = request.params;
+    Book.findByIdAndDelete(book_id)
+      .then((book) => {
+        if (!book) response.status(404).send("Book not found");
+        else response.status(200).send("Done");
+      })
+      .catch((e) => {
+        response.status(500).send(e.message);
+      });
 };
 
 module.exports = {
